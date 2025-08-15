bbbff0db8c6f2b1ddeb88b25e18c1f2006982dc6
L-Mendoza281077 <U24301701@UTP.EDU.PE>
2025-08-15 00:25:58 -0500

    TallerJavaUTP

---
+++ TallerProgramacionUTP/src/main/java/com/mycompany/tallerprogramacionutp/EjerSem01Q2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tallerprogramacionutp;
+
+/**
+ *
+ * @author Luis Mendoza U24301701
+ */
+public class EjerSem01Q2 {
+    public static void main(String[] args) {
+        // Declaración y asignación de variables
+        String nombre = "Luis";
+        int edad = 25;
+        boolean esEstudiante = true;
+
+        // Imprimir valores
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("¿Eres estudiante?: " + esEstudiante);
+    }
+}
