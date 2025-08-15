0acdafae5096ea2b8e32cc802bdbfb4b25df9f4f
L-Mendoza281077 <U24301701@UTP.EDU.PE>
2025-08-15 00:34:07 -0500

    TallerJavaUTP

---
+++ TallerProgramacionUTP/src/main/java/com/mycompany/tallerprogramacionutp/EjerSem01Q3.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tallerprogramacionutp;
+
+/**
+ *
+ * @author Luis Mendoza U24301701
+ */
+public class EjerSem01Q3 {
+    public static void main(String[] args) {
+        // Definir la constante
+        final double PI = 3.1416;
+
+        // Radio del círculo
+        double radio = 5;
+
+        // Calcular el área
+        double area = PI * radio * radio;
+
+        // Mostrar el resultado
+        System.out.println("El área del círculo es: " + area);
+    }
+}
