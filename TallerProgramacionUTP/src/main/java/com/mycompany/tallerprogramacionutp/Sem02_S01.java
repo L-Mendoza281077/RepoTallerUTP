be8cdf5ff5c01cd1b2a1dc0c4015d6a1f83ff860
L-Mendoza281077 <U24301701@UTP.EDU.PE>
2025-08-18 20:51:45 -0500

    f38cc518dd6c7798f5238b66b1f1ee33f5da847b

--- TallerProgramacionUTP/src/main/java/com/mycompany/tallerprogramacionutp/Sem02_S01.java
+++ TallerProgramacionUTP/src/main/java/com/mycompany/tallerprogramacionutp/Sem02_S01.java
@@ -6,19 +6,38 @@ package com.mycompany.tallerprogramacionutp;
 
 /**
  *
- * @author Mariana
+ * @author Luis Mendoza U24301701
  */
 public class Sem02_S01 {
     public static void main(String[] args){
+    //creamos una array del 1 al 5
     int[] nums = {1, 2, 3, 4, 5};  
-    int result = 0;
-    for (int i= 0; i < nums.length; i ++) {
-        if (i % 2 == 0) {
-          result += nums[i];
+    //Declaramos una variable con valor cero
+    int result = 0;                
+    //Bucle for desde i=0 hasta i<nums.lenght, es decir hasta el último
+    //nums.length = 5, así que i toma los valores 0, 1, 2, 3, 4    
+    for (int i= 0; i < nums.length; i ++) {   
+        //Verifica si el índice i es par (% es el operador módulo, que da el residuo de la división).
+        //Ejemplo: 0 % 2 = 0 (par), 1 % 2 = 1 (impar).
+        if (i % 2 == 0) {          // 
+            //Si el índice es par, suma el valor en esa posición al acumulador result.
+            //Ejemplo: cuando i = 0, suma nums[0] que es 1          
+            result += nums[i];
         }else {
-          result += nums[i];
+            //Si el índice es impar, resta el valor en esa posición.
+            //Ejemplo: cuando i = 1, resta nums[1] que es 2.
+            result -= nums[i];
         }
     }
+    //Imprime en consola el valor final de result.
     System.out.println(result);
+    //Ejecución paso a paso con {1,2,3,4,5}
+    //i = 0 (par)   → result =  0 + 1 =  1
+    //i = 1 (impar) → result =  1 - 2 = -1
+    //i = 2 (par)   → result = -1 + 3 =  2
+    //i = 3 (impar) → result =  2 - 4 = -2
+    //i = 4 (par)   → result = -2 + 5 =  3
+    //Al final debe imprime: 3
+  
 }
 }
