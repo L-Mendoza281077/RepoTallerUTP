369b564699ea9429f50efac9a1a37b4da1b9360e
L-Mendoza281077 <U24301701@UTP.EDU.PE>
2025-08-20 19:08:02 -0500

    f38cc518dd6c7798f5238b66b1f1ee33f5da847b

---
+++ TallerProgramacionUTP/src/main/java/com/mycompany/tallerprogramacionutp/EjemploMath01.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tallerprogramacionutp;
+
+/**
+ *
+ * @author Luis Mendoza U24301701
+ */
+public class EjemploMath01 {
+    public static void main (String[]args){
+    double numero = 25.0;
+    double raizcuadrada = Math.sqrt(numero);
+    System.out.println ("Raiz cuadrada de: " + numero + " es:" + raizcuadrada);    
+    }
+    
+}
